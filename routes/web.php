5bd277a0ed171732ac37484dfb4bbd06f395a850
patelmanasi <manasipatel0852@gmail.com>
2026-05-15 10:23:47 +0530

    feat(products): implement cache query dashboard with search, delete, pagination, alerts, analytics cards, and modern dark UI

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-// Show products (with cache)
 Route::get('/', [ProductController::class, 'index']);
 
-// Add product
 Route::post('/add-product', [ProductController::class, 'store']);
+
+Route::delete('/delete-product/{id}', [ProductController::class, 'destroy']);
